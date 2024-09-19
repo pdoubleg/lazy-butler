d691e43eb2f04dee6806cd72be2613406dd8275a
Paul Gargano <pdoubleg@gmail.com>
2024-09-19 01:09:30 -0500

    adds frontend

---
+++ src/env/server.ts
@@ -0,0 +1,19 @@
+import { ZodError, z } from "zod";
+import { createEnv } from "@t3-oss/env-nextjs";
+
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z.enum(["development", "production"]),
+  },
+  onValidationError: (error: ZodError) => {
+    console.error(
+      "❌ Invalid environment variables:",
+      error.flatten().fieldErrors
+    );
+    process.exit(1);
+  },
+  emptyStringAsUndefined: true,
+  // eslint-disable-next-line n/no-process-env
+  experimental__runtimeEnv: process.env,
+});
